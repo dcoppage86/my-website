6e7e53b98fd8e1e201a6a858c81c41035d8f0cfa
Dakota <dakotakcoppage@me.com>
2021-08-18 16:41:56 -0600

    added new styling in css and added components to router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
+import Contact from './components/Contact'
+import Resume from './components/Resume'
+import Home from './components/Home'
+import Portfolio from './components/Portfolio'
 
 
 
@@ -9,12 +13,11 @@ function App() {
     <Router>
       <NavigationBar/>
       <div className="App">
-          <h1>Hello world</h1>
           <Switch>
-            <Route exact path="/"/>
-            <Route exact path="/about"/>
-            <Route exact path="/portfolio"/>
-            <Route exact path="/contact"/>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/Resume" component={Resume}/>
+            <Route exact path="/portfolio" component={Portfolio}/>
+            <Route exact path="/contact" component={Contact}/>
           </Switch>
       </div>
       <Footer />
