bd57ffeb30173ce1c1dc46ea0d5442520d8c8cda
Dakota <dakotakcoppage@me.com>
2021-08-23 10:53:36 -0600

    adding css to home page and updated skills json to add the images to page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import dakota from '../assets/images/dakota.png'
+import SkillsCard from './SkillsCard'
+
 
 function Home() {
     return (
         <Container className="home-container">
             <Row>
-                <Col xs={6} md={4}>
+                <Col Image>
                     <Image src={dakota} roundedCircle />
                 </Col>
-                <Col>
+                <Col md={4}>
                     <h1 className="welcome">
                         HI, I'M <strong className="dakota">DAKOTA</strong>,
                         <br></br>
@@ -18,6 +20,11 @@ function Home() {
                         ENGINEER
                     </h1>
                 </Col>
+            </Row>
+            <br></br>
+            <br></br>
+            <Row>
+                <SkillsCard/>
             </Row>
         </Container>
     )
---
+++ src/components/SkillsCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import skills from '../assets/data/skills.json'
+import { Card } from 'react-bootstrap';
+
+const SkillsCard = () => {
+    return (
+        skills.map(skill => 
+        <Card key={skill.id} className="skill-card">
+            <Card.Img className="card-image" src={skill.image_url} />
+            <Card.Title className="card-content">{skill.name}</Card.Title>
+        </Card>)
+
+    )
+    
+}
+
+export default SkillsCard;
