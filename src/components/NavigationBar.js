dd343742def02908581debdc84dde0da86ada96b
Dakota <dakotakcoppage@me.com>
2021-08-24 00:59:13 -0600

    added classnames and active status

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -11,16 +11,18 @@ import { Link, NavLink } from 'react-router-dom';
 const NavigationBar = () => {
     return (
         <Navbar className="nav-bar">
-            <NavLink className="navbar-brand" exact to="/"><strong>|DC|</strong></NavLink>
+            <NavLink className="navbar-brand" activeClassName="underline" exact to="/"><strong>|DC|</strong></NavLink>
             <Nav className="me-auto">
-                <Nav.Link as={Link} exact to="/portfolio">Portfolio</Nav.Link>
-                <Nav.Link as={Link} exact to="/resume">Resume</Nav.Link>
-                <Nav.Link as={Link} exact to="/contact">Contact</Nav.Link>
+                <Nav.Link as={Link} activeClassName="underline" exact to="/portfolio">PORTFOLIO</Nav.Link>
+                <br></br>
+                <Nav.Link as={Link} activeClassName="underline" exact to="/resume">RESUME</Nav.Link>
+                <br></br>
+                <Nav.Link as={Link} activeClassName="underline" exact to="/contact">CONTACT</Nav.Link>
             </Nav>
-            <Navbar.Brand className="github" href="https://github.com/dcoppage86"><FaGithub size={28}/></Navbar.Brand>
-            <Navbar.Brand className="linked-in" href="https://www.linkedin.com/in/dkc86"><FaLinkedin size={28}/></Navbar.Brand>
-            <Navbar.Brand className="twitter" href="www.linkedin.com/in/dkc86"><FaTwitter size={28}/></Navbar.Brand>
-            <Navbar.Brand className="medium" href="https://dakotakcoppage.medium.com/"><FaMediumM size={28}/></Navbar.Brand>
+            <Navbar.Brand className="navbar-brand" href="https://github.com/dcoppage86"><FaGithub size={28}/></Navbar.Brand>
+            <Navbar.Brand className="navbar-brand" href="https://www.linkedin.com/in/dkc86"><FaLinkedin size={28}/></Navbar.Brand>
+            <Navbar.Brand className="navbar-brand" href="www.linkedin.com/in/dkc86"><FaTwitter size={28}/></Navbar.Brand>
+            <Navbar.Brand className="navbar-brand" href="https://dakotakcoppage.medium.com/"><FaMediumM size={28}/></Navbar.Brand>
         </Navbar>
     );
 }
