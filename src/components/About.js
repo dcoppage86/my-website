c78faca7158838f11b47f736b1fe6a772608dce4
Dakota <dakotakcoppage@me.com>
2021-08-25 20:23:19 -0600

    added styling for p tag and added class name to p

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <h1>A Little About Me</h1>
             <br></br>
             <br></br>
-            <p>Duis incididunt esse non elit ullamco labore et veniam nulla. Sit id culpa officia non magna culpa et dolor ea cupidatat. Dolor non ullamco velit eiusmod do enim est excepteur adipisicing aliquip. Qui non est cupidatat id duis voluptate in. Adipisicing enim Lorem dolor minim. Eu tempor laboris anim do aliquip eu cupidatat eiusmod sunt.
+            <p className="about-content">Duis incididunt esse non elit ullamco labore et veniam nulla. Sit id culpa officia non magna culpa et dolor ea cupidatat. Dolor non ullamco velit eiusmod do enim est excepteur adipisicing aliquip. Qui non est cupidatat id duis voluptate in. Adipisicing enim Lorem dolor minim. Eu tempor laboris anim do aliquip eu cupidatat eiusmod sunt.
 
 Dolore aliquip excepteur et minim. Sunt sit in do ad incididunt Lorem sit. Duis ipsum officia exercitation sunt occaecat exercitation laboris dolor esse nisi non. Commodo ex elit Lorem ex. Lorem et proident non sit culpa in dolore quis deserunt reprehenderit et minim veniam minim. Dolor sunt irure sint et magna voluptate magna ex non. Amet sit officia aliqua veniam veniam ut enim laboris tempor sint duis adipisicing labore exercitation.
 
