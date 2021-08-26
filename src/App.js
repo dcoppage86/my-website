567b2d54c9e992aac29db74ceba3bc3e8b5208ac
Dakota <dakotakcoppage@me.com>
2021-08-25 18:08:48 -0600

    imported about component to render in the app structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import Resume from './components/Resume'
 import Home from './components/Home'
 import Portfolio from './components/Portfolio'
-
+import About from './components/About';
 
 
 
