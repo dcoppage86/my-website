9610523f9c930cf8a0cfa72daa788e8d5e29bffd
Dakota <dakotakcoppage@me.com>
2021-08-25 18:13:42 -0600

    created about component with some dummy text to see the layout

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="about-div">
+            <h1>A Little About Me</h1>
+            <br></br>
+            <br></br>
+            <p>Duis incididunt esse non elit ullamco labore et veniam nulla. Sit id culpa officia non magna culpa et dolor ea cupidatat. Dolor non ullamco velit eiusmod do enim est excepteur adipisicing aliquip. Qui non est cupidatat id duis voluptate in. Adipisicing enim Lorem dolor minim. Eu tempor laboris anim do aliquip eu cupidatat eiusmod sunt.
+
+Dolore aliquip excepteur et minim. Sunt sit in do ad incididunt Lorem sit. Duis ipsum officia exercitation sunt occaecat exercitation laboris dolor esse nisi non. Commodo ex elit Lorem ex. Lorem et proident non sit culpa in dolore quis deserunt reprehenderit et minim veniam minim. Dolor sunt irure sint et magna voluptate magna ex non. Amet sit officia aliqua veniam veniam ut enim laboris tempor sint duis adipisicing labore exercitation.
+
+Ut duis excepteur laboris excepteur proident voluptate. Cupidatat enim incididunt commodo nulla est dolore sint. Aute consequat veniam do Lorem anim ad. Ut dolor reprehenderit sunt ullamco deserunt incididunt aliqua occaecat id ex. Nostrud consectetur do ad officia excepteur anim voluptate quis sit pariatur enim.
+
+Ullamco deserunt commodo nostrud ex elit cillum. Proident duis aute ea excepteur nostrud laborum eiusmod consequat elit Lorem consequat. Proident sunt id magna cillum amet officia qui. Incididunt cillum minim esse cillum qui ut. Non commodo excepteur anim proident commodo irure sit cillum nostrud est ipsum. Labore velit dolore et nostrud.</p>
         </div>
     );
 }
