72082db684b27fe222fd48cc5f01b654ff9a4e0f
Dakota <dakotakcoppage@me.com>
2021-08-30 16:27:30 -0600

    made navbar more responsive with hamburger menu

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav} from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { FaGithub } from 'react-icons/fa'
 import { FaLinkedin } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa'; 
@@ -10,19 +11,22 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavigationBar = () => {
     return (
-        <Navbar className="nav-bar">
+        <Navbar className="nav-bar" expand="lg">
             <NavLink className="navbar-brand" activeClassName="underline" exact to="/"><strong>|DC|</strong></NavLink>
-            <Nav className="me-auto">
-                <Nav.Link as={Link} activeClassName="underline" exact to="/portfolio">PORTFOLIO</Nav.Link>
-                <br></br>
-                <Nav.Link as={Link} activeClassName="underline" exact to="/resume">RESUME</Nav.Link>
-                <br></br>
-                <Nav.Link as={Link} activeClassName="underline" exact to="/about">ABOUT</Nav.Link>
-            </Nav>
-            <Navbar.Brand className="navbar-brand" href="https://github.com/dcoppage86"><FaGithub size={28}/></Navbar.Brand>
-            <Navbar.Brand className="navbar-brand" href="https://www.linkedin.com/in/dkc86"><FaLinkedin size={28}/></Navbar.Brand>
-            <Navbar.Brand className="navbar-brand" href="www.linkedin.com/in/dkc86"><FaTwitter size={28}/></Navbar.Brand>
-            <Navbar.Brand className="navbar-brand" href="https://dakotakcoppage.medium.com/"><FaMediumM size={28}/></Navbar.Brand>
+            <Navbar.Toggle/>
+            <NavbarCollapse>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} activeClassName="underline" exact to="/portfolio">PORTFOLIO</Nav.Link>
+                    <br></br>
+                    <Nav.Link as={Link} activeClassName="underline" exact to="/resume">RESUME</Nav.Link>
+                    <br></br>
+                    <Nav.Link as={Link} activeClassName="underline" exact to="/about">ABOUT</Nav.Link>
+                </Nav>
+                <Navbar.Brand className="navbar-brand" href="https://github.com/dcoppage86"><FaGithub size={28}/></Navbar.Brand>
+                <Navbar.Brand className="navbar-brand" href="https://www.linkedin.com/in/dkc86"><FaLinkedin size={28}/></Navbar.Brand>
+                <Navbar.Brand className="navbar-brand" href="www.linkedin.com/in/dkc86"><FaTwitter size={28}/></Navbar.Brand>
+                <Navbar.Brand className="navbar-brand" href="https://dakotakcoppage.medium.com/"><FaMediumM size={28}/></Navbar.Brand>
+            </NavbarCollapse>
         </Navbar>
     );
 }
