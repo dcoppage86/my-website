9abe829a5cf72c775e887aafe2d98976daeba377
Brian Franklin <brianfranklinmusic@gmail.com>
2021-11-13 00:36:04 -0500

    Brian's suggestions

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,61 +1,81 @@
-import React, { useState } from 'react'
-import './Contact.css'
-import { useRef } from 'react'
-import emailjs from 'emailjs-com';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone, faEnvelope, faMapMarker } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import "./Contact.css";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarker,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
-    const formRef = useRef();
-    const [formData, setFormData] = useState(false);
+  const formRef = useRef();
+  const [formData, setFormData] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        emailjs.sendForm('service_soagcoc', 'template_sun4uvl', formRef.current, 'user_nbVQ01hXlNAB5h2yhEh0k')
-      .then((result) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_soagcoc",
+        "template_sun4uvl",
+        formRef.current,
+        "user_nbVQ01hXlNAB5h2yhEh0k"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-            setFormData(true);
-      }, (error) => {
+          setFormData(true);
+        },
+        (error) => {
           console.log(error.text);
-      });
-    }
+        }
+      );
+  };
 
-    return (
-        <div className="c">
-            <div className="c-background"></div>
-            <div className="c-wrapper">
-                <div className="c-left">
-                    <h1 className="c-title">Get In Touch</h1>
-                    <div className="c-info-item">
-                        <div className="c-icon"><FontAwesomeIcon icon={faPhone} /> (270) 312-7746</div>
-                    </div>
-                    <div className="c-info-item">
-                        <div className="c-icon"><FontAwesomeIcon icon={faEnvelope} /> dakotacoppagedev@gmail.com</div>
-                    </div>
-                    <div className="c-info-item">
-                        <div className="c-icon"><FontAwesomeIcon icon={faMapMarker} /> Denver, CO</div>
-                    </div>
-                </div>
-                <div className="c-right">
-                    <p className="c-desc">
-                        <b>You Can Contact Me Directly Through Here As Well</b> Let's get in touch!
-                    </p>
-                    <form ref={formRef} onSubmit={handleSubmit} className="c-form">
-                        <input type="text" placeholder="Name" name="user_name"/>
-                        <br></br>
-                        <input type="text" placeholder="Subject" name="user_subject"/>
-                        <br></br>
-                        <input type="text" placeholder="Email" name="user_email"/>
-                        <br></br>
-                        <textarea rows="5" name="message" placeholder="Message"></textarea>
-                        <br></br>
-                        <button>Submit</button>
-                        {formData && <p className="c-form-success">Message Sent!</p>}
-                    </form>
-                </div>
+  return (
+    <div className="c">
+      <div className="c-background"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Get In Touch</h1>
+          <div className="c-info-item">
+            <div className="c-icon">
+              <FontAwesomeIcon icon={faPhone} /> (270) 312-7746
             </div>
+          </div>
+          <div className="c-info-item">
+            <div className="c-icon">
+              <FontAwesomeIcon icon={faEnvelope} /> dakotacoppagedev@gmail.com
+            </div>
+          </div>
+          <div className="c-info-item">
+            <div className="c-icon">
+              <FontAwesomeIcon icon={faMapMarker} /> Denver, CO
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="c-right">
+          <p className="c-desc">
+            <b>You can contact me directly through here as well</b> Let's get in
+            touch!
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit} className="c-form">
+            <input type="text" placeholder="Name" name="user_name" />
+            <br></br>
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <br></br>
+            <input type="text" placeholder="Email" name="user_email" />
+            <br></br>
+            <textarea rows="5" name="message" placeholder="Message"></textarea>
+            <br></br>
+            <button>Submit</button>
+            {formData && <p className="c-form-success">Message Sent!</p>}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import './Intro.css'
-import DKCDEV from '../../assets/images/DKCDEV.png'
+import React from "react";
+import "./Intro.css";
+import DKCDEV from "../../assets/images/DKCDEV.png";
 
 const Intro = () => {
   return (
-        <div className="i">
+    <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My name is</h2>
           <h1 className="i-name">Dakota Coppage</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
-                <div className="i-title-item">Full Stack Software Engineer</div>
-                <div className="i-title-item">Black Coffee Drinker</div>
-                <div className="i-title-item">Web Developer</div>
-                <div className="i-title-item">Father & Husband</div>
-                <div className="i-title-item">Musician & Creator</div>
+              <div className="i-title-item">Full Stack Software Engineer</div>
+              <div className="i-title-item">Black Coffee Drinker</div>
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Father & Husband</div>
+              <div className="i-title-item">Musician & Creator</div>
             </div>
           </div>
           <p className="i-desc">
-            I am a full stack software engineer with a passion for creating accessible web applications. I am a father and husband, and I love to play music, write and play with my dogs.
+            I am a full stack software engineer with a passion for creating
+            accessible web applications. I am a father and husband, and I love
+            to play music, write and play with my dogs.
           </p>
         </div>
-        </div>
-        <div className="i-right">
-            <div className="i-background"></div>
-            <img src={DKCDEV} alt="Dakota Coppage" className="i-img" />
-        </div>
+      </div>
+      <div className="i-right">
+        <img src={DKCDEV} alt="Dakota Coppage" className="i-img" />
+      </div>
     </div>
-    
   );
 };
 
